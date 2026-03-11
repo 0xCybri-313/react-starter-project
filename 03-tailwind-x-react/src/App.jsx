bf4740bfa597ae98db7119d201af2fc225ba883b
0xCybri-313 <r.khursheedkhawar@gmail.com>
2026-03-11 23:17:32 +0500

    Add property cards and implement React props

--- 03-tailwind-x-react/src/App.jsx
+++ 03-tailwind-x-react/src/App.jsx
@@ -2,15 +2,34 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className="flex animate-bounce rounded-md border-2 border-white p-4">
+      <h1 className="m-auto flex w-fit animate-bounce rounded-md border-2 border-white p-4">
         Tailwind Test
       </h1>
+      <div className="flex gap-2">
+        <Card
+          price={200}
+          address="123 Wallaby Avenue, Park Road"
+          bedroom={4}
+          bathroom={2}
+        />
+        <Card
+          price={400}
+          address="978 Hell's Kitchen, New York"
+          bedroom={8}
+          bathroom={3}
+        />
+        <Card
+          price={600}
+          address="40th 10th Street, Down Town"
+          bedroom={12}
+          bathroom={6}
+        />
+      </div>
     </>
   );
 }
