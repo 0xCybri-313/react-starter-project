5b1d191a6b14a3ca2fb534555b0f4ff9dc31295a
0xCybri-313 <r.khursheedkhawar@gmail.com>
2026-04-17 23:59:31 +0500

    add: Header and Main file

--- B-03-meme-generator/src/App.jsx
+++ B-03-meme-generator/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import Header from "../components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-7xl">Hello meme generator</h1>
+      <Header />
     </>
   );
 }
