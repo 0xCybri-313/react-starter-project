bb3db0f495ce0bba72c6648d7dd3390c4b456321
0xCybri-313 <r.khursheedkhawar@gmail.com>
2026-04-30 22:45:24 +0500

    init: create vite project and config tailwindcss for Assembly: Endgame

---
+++ B-05-assembly-endgame/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center justify-center bg-[#282726] text-2xl text-white">
+        <h1 className="">Assembly: Endgame</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
