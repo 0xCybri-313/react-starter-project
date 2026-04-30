68ed2541a277e80e6a1ea2190148ea26d7bac889
0xCybri-313 <r.khursheedkhawar@gmail.com>
2026-04-30 23:50:28 +0500

    add: completed header section and language section

--- B-05-assembly-endgame/src/App.jsx
+++ B-05-assembly-endgame/src/App.jsx
@@ -1,9 +1,48 @@
+import { languages } from "./database/languages";
+
+/**
+ * Goal: Build out the main parts of our app
+ *
+ * Challenge:
+ * 1. Save a "currentWord" in state. Initialize as "react".
+ * 2. Map over the letters of the word (you'll need to turn
+ *    the string into an array of letters first) and display
+ *    each one as a <span>. Capitalize the letters when
+ *    displaying them.
+ * 3. Style to look like the design. You can get the underline
+ *    effect on the box using `border-bottom`.
+ */
+
 function App() {
+  const [currentWord, setCurrentWord] = useState("react");
   return (
     <>
-      <div className="flex h-screen w-screen items-center justify-center bg-[#282726] text-2xl text-white">
-        <h1 className="">Assembly: Endgame</h1>
-      </div>
+      <main className="flex h-screen w-screen flex-col items-center justify-center gap-8 bg-[#282726] text-2xl text-white">
+        <header className="flex h-36 w-sm flex-col items-center">
+          <h1 className="text-xl font-medium text-[#F9F4DA]">
+            Assembly: Endgame
+          </h1>
+          <p className="text-center text-sm font-medium text-[#8E8E8E]">
+            Guess the word within 8 attempts to keep the programming world safe
+            from Assembly!
+          </p>
+          <section className="mt-4 flex h-full w-full flex-col items-center justify-center rounded bg-[#10A95B] text-[#F9F4DA]">
+            <h2 className="text-xl font-medium">You win!</h2>
+            <p className="text-sm font-medium">Well done! 🎉</p>
+          </section>
+        </header>
+        <section className="flex w-xs flex-wrap items-center justify-center gap-1">
+          {languages.map((lang) => (
+            <div
+              className="flex h-6 items-center justify-center rounded px-2 text-xs font-bold"
+              style={{ background: lang.backgroundColor, color: lang.color }}
+              key={lang.name}
+            >
+              {lang.name}
+            </div>
+          ))}
+        </section>
+      </main>
     </>
   );
 }
