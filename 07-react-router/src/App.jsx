89230df0b4c4303c5e18e93beefd1dc65f0adb1e
Khursheed Khawar <r.khursheedkhawar@gmail.com>
2026-05-09 00:36:08 +0500

    Remove unused useState for count
    Removed unused state management for count.

--- 07-react-router/src/App.jsx
+++ 07-react-router/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import { Outlet } from "react-router";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
